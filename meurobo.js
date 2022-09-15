64a53ec139599bf2dd41ce34d405ae9311a080d3
Caio Rabelo <caiorabelo020@gmail.com>
2022-09-15 18:27:35 -0300

    estrategiabranco

--- meurobo.js
+++ meurobo.js
@@ -1,11 +1,15 @@
 const
     url = "https://api2.minhablaze.com.br/api/v1/result/double"
 var
+aux = 1
+estrategia = 0
+estrategia_branco = 0
     lem = (el) => {
         return document.querySelector(el);
     },
-sugestao = lem('.sugestao'),
+    sugestao = lem('.sugestao'),
     startRobo = () => {
+
         //inicio da nosa Fetch API
         fetch(
             url,
@@ -18,23 +22,54 @@ sugestao = lem('.sugestao'),
             //Inicio das funcoes do robo
             (r) => {
                 resultados = r.results
-                casa = 15
-                tamanho = resultados.length
-                indice = tamanho - casa
-                cor = resultados[indice].color
-                //converter valores das cores
-                switch (cor) {
-                    case 1:
-                        cor = 'VERMELHO'
-                        break
-                    case 2:
-                        cor = 'PRETO'
-                        break
-                    case 0:
-                        cor = 'BRANCO'
-                        break
+                tamanho = resultados.length - 1
+                dados = resultados.color
+                if (estrategia == 1) {
+                    if (cor_num == resultados[(tamanho)].color) {
+                        acerto = 1
+                        estrategia_branco = 0
+                        estrategia = 0
+                    } else {
+                        acerto = 0
+                    }
                 }
-                lem('.sugestao').innerHTML = cor
+                if (((resultados[(tamanho - 1)].color == 0) && (resultados[(tamanho)].color != 0)) || estrategia_branco == 1) {
+                    estrategia = 1
+                    if (estrategia_branco == 0) {
+                        if (resultados[(tamanho)].color == 1) {
+                            cor = 2
+                        } else {
+                            cor = 1
+                        }
+                    }else{
+                        if (resultados[(tamanho-1)].color == 1) {
+                            cor = 2
+                        } else {
+                            cor = 1
+                        }
+                    }
+                }
+                cor_num = cor
+                    // 
+                    // Aplicar a logica de aposta (valor de cada aposta)
+                    //
+                //converter valores das cores
+                if (estrategia == 1) {
+                    switch (cor) {
+                        case 1:
+                            cor = 'VERMELHO'
+                            break
+                        case 2:
+                            cor = 'PRETO'
+                            break
+                        case 0:
+                            cor = 'BRANCO'
+                            break
+                    }
+                } else {
+                    cor = 'Nada'
+                }
+                lem('.sugestao').innerHTML = aux
             }
         )
         setTimeout(() => { startRobo() }, 3000)
