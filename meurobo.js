cd4478f20b7348f990df3c48e24df4775c09bd7e
Caio Rabelo <caiorabelo020@gmail.com>
2022-09-21 11:09:38 -0300

    new

--- meurobo.js
+++ meurobo.js
@@ -29,6 +29,119 @@ lem = (el) => {
                 resultados = r.results
                 tamanho = resultados.length - 1
                 dados = resultados.color
+                analise_branco1p = 0
+                analise_branco1p_protecao = 0
+                analise_branco2p = 0
+                analise_branco2p_protecao = 0
+                branco_loss = 0
+                analise_tbt1p = 0
+                analise_tbt1p_protecao = 0
+                analise_tbt2p = 0
+                analise_tbt2p_protecao = 0
+                tbt_loss = 0
+                analise_alternador1p = 0
+                analise_alternador1p_protecao = 0
+                analise_alternador2p = 0
+                analise_alternador2p_protecao = 0
+                alternador_loss = 0
+                for(i=0;i<=(tamanho - 3);i++){
+                    //branco
+                    if((resultados[i].color == 0) && (resultados[(i+1)].color != 0)){
+                        if (resultados[(i+1)].color == 1) {
+                            if(resultados[(i+2)].color == 2){
+                                analise_branco1p = analise_branco1p + 1
+                            }else if(resultados[(i+2)].color == 0){
+                                analise_branco1p_protecao = analise_branco1p_protecao + 1
+                            }else{
+                                if(resultados[(i+3)].color==2){
+                                    analise_branco2p = analise_branco2p + 1
+                                }else if(resultados[(i+3)].color==0){
+                                    analise_branco2p_protecao = analise_branco2p_protecao + 1
+                                }else{
+                                    branco_loss = branco_loss + 1
+                                }
+                            }
+                        }else if (resultados[(i+1)].color == 2) {
+                            if(resultados[(i+2)].color == 1){
+                                analise_branco1p = analise_branco1p + 1
+                            }else if(resultados[(i+2)].color == 0){
+                                analise_branco1p_protecao = analise_branco1p_protecao + 1
+                            }else{
+                                if(resultados[(i+3)].color==1){
+                                    analise_branco2p = analise_branco2p + 1
+                                }else if(resultados[(i+3)].color==0){
+                                    analise_branco2p_protecao = analise_branco2p_protecao + 1
+                                }else{
+                                    branco_loss = branco_loss + 1
+                                }
+                            }
+                        }
+                    }
+                    // tbt
+                    if(i<=(tamanho - 6) && resultados[(i+4)].color != resultados[(i+3)].color && resultados[(i+4)].color != 0 && (resultados[(i+3)].color == resultados[(i+2)].color && resultados[(i+2)].color == resultados[(i+1)].color && resultados[(i+1)].color == resultados[(i)].color)){
+                        if(resultados[(i+3)].color == 1){
+                            if(resultados[(i+5)].color == 1){
+                                analise_tbt1p = analise_tbt1p + 1
+                            }else if(resultados[(i+5)].color == 0){
+                                analise_tbt1p_protecao = analise_tbt1p_protecao + 1
+                            }else{
+                                if(resultados[(i+6)].color==1){
+                                    analise_tbt2p = analise_tbt2p + 1
+                                }else if(resultados[(i+6)].color==0){
+                                    analise_tbt2p_protecao = analise_tbt2p_protecao + 1
+                                }else{
+                                    tbt_loss = tbt_loss + 1
+                                }
+                            }
+                        }else if(resultados[(i+3)].color == 2){
+                            if(resultados[(i+5)].color == 2){
+                                analise_tbt1p = analise_tbt1p + 1
+                            }else if(resultados[(i+5)].color == 0){
+                                analise_tbt1p_protecao = analise_tbt1p_protecao + 1
+                            }else{
+                                if(resultados[(i+6)].color==2){
+                                    analise_tbt2p = analise_tbt2p + 1
+                                }else if(resultados[(i+6)].color==0){
+                                    analise_tbt2p_protecao = analise_tbt2p_protecao + 1
+                                }else{
+                                    tbt_loss = tbt_loss + 1
+                                }
+                            }
+                        }
+                    }
+                    // quebra de alternancia
+                    if (i <= (tamanho-6) && (resultados[(i+4)].color != 0) && (resultados[i+4].color == resultados[(i+3)].color && resultados[(i+3)].color == resultados[(i+1)].color) && (resultados[(i+2)].color == resultados[(i)].color) && (resultados[(i)].color != resultados[(i+1)].color)) {
+                        if(resultados[(i+2)].color == 1){
+                            if (resultados[(i + 5)].color == 1) {
+                                analise_alternador1p = analise_alternador1p + 1
+                            } else if (resultados[(i + 5)].color == 0) {
+                                analise_alternador1p_protecao = analise_alternador1p_protecao + 1
+                            } else {
+                                if (resultados[(i + 6)].color == 1) {
+                                    analise_alternador2p = analise_alternador2p + 1
+                                } else if (resultados[(i + 6)].color == 0) {
+                                    analise_alternador2p_protecao = analise_alternador2p_protecao + 1
+                                } else {
+                                    alternador_loss = alternador_loss + 1
+                                }
+                            }
+                        }else if(resultados[(i+2)].color == 2){
+                            if (resultados[(i + 5)].color == 2) {
+                                analise_alternador1p = analise_alternador1p + 1
+                            } else if (resultados[(i + 5)].color == 0) {
+                                analise_alternador1p_protecao = analise_alternador1p_protecao + 1
+                            } else {
+                                if (resultados[(i + 6)].color == 2) {
+                                    analise_alternador2p = analise_alternador2p + 1
+                                } else if (resultados[(i + 6)].color == 0) {
+                                    analise_alternador2p_protecao = analise_alternador2p_protecao + 1
+                                } else {
+                                    alternador_loss = alternador_loss + 1
+                                }
+                            }
+                        }
+                    }
+                }
                 if(apostando == 1 && id_aposta1 != resultados[tamanho].id){
                     // entra nesse if quando a jogada 1 foi realizada
                     if(num_cor == resultados[tamanho].color){
@@ -114,15 +227,19 @@ lem = (el) => {
                 }else {
                     cor = 'Nada'
                 }
-                lem('.sugestao').innerHTML = [cor, " Aposta: ", apostando, " Acertos de primeira: ", acerto1, " Acertos de segunda: ", acerto2, " Acertos branco de primeira: ", acerto1_branco, " Acertos branco de segunda: ", acerto2_branco, " Perdas: ", perdas]
+                
+                
+//                lem('.sugestao').innerHTML = [cor, " Aposta: ", apostando, " Acertos de primeira: ", acerto1, " Acertos de segunda: ", acerto2, " Acertos branco de primeira: ", acerto1_branco, " Acertos branco de segunda: ", acerto2_branco, " Perdas: ", perdas]
+ //                 lem('.sugestao').innerHTML = [ "Branco Principal: ",analise_branco1p, "Branco1 proteçao: ",analise_branco1p_protecao,"Branco Gale: ",analise_branco2p,"Branco Gale proteçao: ", analise_branco2p_protecao,"Branco Loss: ",branco_loss]
+ //                 lem('.sugestao').innerHTML = [ "TBT Principal: ",analise_tbt1p, "TBT1 proteçao: ",analise_tbt1p_protecao,"TBT Gale: ",analise_tbt2p,"TBT Gale proteçao: ", analise_tbt2p_protecao,"TBT Loss: ",tbt_loss]
+                  lem('.sugestao').innerHTML = [ "Alternador Principal:" + analise_alternador1p + "<br>Alternador1 proteçao: " + analise_alternador1p_protecao + "<br>Alternador Gale: " + analise_alternador2p + "<br>Alternador Gale proteçao: " + analise_alternador2p_protecao + "<br>Alternador Loss: " + alternador_loss]
+//                  lem('.sugestao').innerHTML = [a]
             }
         )
         setTimeout(() => { startRobo() }, 3000)
     }
 
 startRobo()
-
-
 
 
 
@@ -151,3 +268,15 @@ startRobo()
 //                        } else {
 //                            cor = 1
 //                        }
+
+
+
+
+
+
+
+
+
+
+
+
